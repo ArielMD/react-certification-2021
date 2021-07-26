c432bed3423c46869ce2270aecba907b9ef1ff6a
Ariel May <aemd199711@gmail.com>
2021-07-26 16:24:32 -0500

    test: sidebar component unit test

---
+++ src/components/Sidebar/sidebar.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+
+import Sidebar from './Sidebar.component';
+
+beforeEach(() => {
+  render(
+    <BrowserRouter>
+      <Switch>
+        <Route>
+          <div id="main-container">
+            <Sidebar hiddeSidebar />
+          </div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+});
+
+describe('Sidebar component', () => {
+  it('should contains home', () => {
+    const homeElement = screen.queryByText(/Home/i);
+
+    expect(homeElement).toBeInTheDocument();
+  });
+  it('should contains playlist', () => {
+    const playlistElement = screen.queryByText(/Playlist/i);
+
+    expect(playlistElement).toBeInTheDocument();
+  });
+  it('should contains history', () => {
+    const historyElement = screen.queryByText(/History/i);
+
+    expect(historyElement).toBeInTheDocument();
+  });
+});
