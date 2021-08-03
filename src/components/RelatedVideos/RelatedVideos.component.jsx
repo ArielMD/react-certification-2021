7901469eba3a9d3b690e0349b9ac2dce327c831f
Ariel May <aemd199711@gmail.com>
2021-08-03 02:12:51 -0500

    feat: add related videos component

---
+++ src/components/RelatedVideos/RelatedVideos.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import useRelatedVideos from '../../utils/hooks/useRelatedVideos';
+import useQuery from '../../utils/hooks/useQuery';
+import VideoCard from '../VideoCard';
+
+const RelatedVideos = () => {
+  const query = useQuery();
+  const { videos, error } = useRelatedVideos(query.get('id'));
+
+  return (
+    <div>
+      {error && <h3>Error</h3>}
+      {videos?.items.map(
+        (item) =>
+          item.snippet && (
+            <VideoCard
+              key={item.id.videoId}
+              title={item.snippet.title}
+              description={item.snippet.description}
+              thumbnails={item.snippet.thumbnails}
+              channelTitle={item.snippet.channelTitle}
+            />
+          )
+      )}
+    </div>
+  );
+};
+
+export default RelatedVideos;
