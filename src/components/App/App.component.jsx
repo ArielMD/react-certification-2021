9a163ec682a0bc22eb7d7d82aeac3a2ed68b4797
Ariel May <aemd199711@gmail.com>
2021-08-16 18:10:40 -0500

    delete unused files

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import AuthProvider from '../../providers/Auth';
 import HOCThemeProvider from '../../providers/Theme/Theme.provider';
 import HomePage from '../../pages/Home';
 import SearchPage from '../../pages/Search';
@@ -11,26 +10,24 @@ import Layout from '../Layout';
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <HOCThemeProvider>
-          <Layout>
-            <Switch>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
-              <Route exact path="/search">
-                <SearchPage />
-              </Route>
-              <Route exact path="/video">
-                <VideoDetails />
-              </Route>
-              <Route path="*">
-                <NotFound />
-              </Route>
-            </Switch>
-          </Layout>
-        </HOCThemeProvider>
-      </AuthProvider>
+      <HOCThemeProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route exact path="/search">
+              <SearchPage />
+            </Route>
+            <Route exact path="/video">
+              <VideoDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Layout>
+      </HOCThemeProvider>
     </BrowserRouter>
   );
 }
